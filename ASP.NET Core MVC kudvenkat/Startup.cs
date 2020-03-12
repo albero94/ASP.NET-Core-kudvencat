aa45237165dae8fde19f1f554bb5df515a40c471
albero94 <ealbero94@gmail.com>
2020-03-12 18:09:14 -0400

    feat: Add MVC services and middleware
    Also create home controller to see how it handles home calls but about calls would go to the Run middleware.

--- ASP.NET Core MVC kudvenkat/Startup.cs
+++ ASP.NET Core MVC kudvenkat/Startup.cs
@@ -16,6 +16,7 @@ namespace ASP.NET_Core_MVC_kudvenkat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,9 +29,11 @@ namespace ASP.NET_Core_MVC_kudvenkat
 
             app.UseStaticFiles();
 
+            app.UseMvcWithDefaultRoute();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hosting Environment: " + env.EnvironmentName);
+                await context.Response.WriteAsync("Hello world!");
             });
 
             
