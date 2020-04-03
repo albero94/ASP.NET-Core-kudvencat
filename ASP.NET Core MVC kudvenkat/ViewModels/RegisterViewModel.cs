8fb37a8e228b3683987c3fe27d1e9797f9e2c5da
albero94 <ealbero94@gmail.com>
2020-04-03 17:34:53 -0400

    feat: Part 67 - UserManager and SignInManager

---
+++ ASP.NET Core MVC kudvenkat/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_kudvenkat.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage ="Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
