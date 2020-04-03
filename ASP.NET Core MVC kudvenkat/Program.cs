9c90c5c380067316b7ed15ec06248fe6b57ad2ef
albero94 <ealbero94@gmail.com>
2020-04-02 20:19:40 -0400

    feat: Part 63 - Logging to a file using 3rd party provider

--- ASP.NET Core MVC kudvenkat/Program.cs
+++ ASP.NET Core MVC kudvenkat/Program.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 
 namespace ASP.NET_Core_MVC_kudvenkat
 {
@@ -16,11 +18,16 @@ namespace ASP.NET_Core_MVC_kudvenkat
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+                logging.AddEventSourceLogger();
+                logging.AddNLog();
+            })
+            .UseStartup<Startup>();
     }
 }
